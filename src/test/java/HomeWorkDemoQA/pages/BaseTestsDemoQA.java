fe5c14dbb138c86feb4c49b64d5cd876f5fe075e
Maksim010 <max_dro01@mail.ru>
2025-07-13 15:10:51 +0300

    tasks.register(SMOKE, TEST)

--- src/test/java/HomeWorkDemoQA/pages/BaseTestsDemoQA.java
+++ src/test/java/HomeWorkDemoQA/pages/BaseTestsDemoQA.java
@@ -1,10 +1,12 @@
 package HomeWorkDemoQA.pages;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -28,7 +30,7 @@ public class BaseTestsDemoQA {
         Configuration.baseUrl = BASE_URL;
         Configuration.browser = BROWSER;
         Configuration.pageLoadTimeout = PAGE_LOAD_TIMEOUT;
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.timeout = TIMEOUT;
         SelenideLogger.addListener("AllureListener" , new AllureSelenide());
     }
@@ -40,5 +42,10 @@ public class BaseTestsDemoQA {
         PRACTICE_FORM_TEXT_SELECTOR.shouldHave(text(PRACTICE_FORM_TEXT));
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Selenide.closeWebDriver();
     }
 }
